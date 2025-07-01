2381362ec0f94d148f1089eac349660eff526ec0
A.Ahmed <94906867+AAhmed-SD@users.noreply.github.com>
2025-07-01 14:14:08 +0100

    fix: add default export to login page for Next.js build

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,9 @@
-<p style={{textAlign: 'right', marginTop: 8}}>
-  <a href="/forgot-password">Forgot Password?</a>
-</p> 
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <p style={{textAlign: 'right', marginTop: 8}}>
+      <a href="/forgot-password">Forgot Password?</a>
+    </p>
+  );
+} 
